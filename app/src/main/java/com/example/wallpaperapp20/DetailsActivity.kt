b425ee40a1928377c387198f1e286a7ba613245c
kekskn <knezevicksenija42@gmail.com>
2023-11-09 15:03:39 +0100

    added time

--- app/src/main/java/com/example/wallpaperapp20/DetailsActivity.kt
+++ app/src/main/java/com/example/wallpaperapp20/DetailsActivity.kt
@@ -1,7 +1,11 @@
 package com.example.wallpaperapp20
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class DetailsActivity : FragmentActivity() {
 
@@ -13,7 +17,13 @@ class DetailsActivity : FragmentActivity() {
                 .replace(R.id.details_fragment, VideoDetailsFragment())
                 .commitNow()
         }
+        val timeTextView: TextView = findViewById(R.id.timeTextView)
+
+        val currentTime = SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date())
+
+        timeTextView.text = currentTime
     }
+
 
     companion object {
         const val SHARED_ELEMENT_NAME = "hero"
